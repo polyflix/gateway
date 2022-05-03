87dccdbeab7dab4adc6764c0a3712f273b825ebc
thomas.gouveia <thomas.gouveia@etu.umontpellier.fr>
2022-05-03 10:14:28 +0200

    chore: init project

---
+++ src/main/kotlin/fr/polyflix/gateway/GatewayApplication.kt
@@ -0,0 +1,11 @@
+package fr.polyflix.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<GatewayApplication>(*args)
+}
